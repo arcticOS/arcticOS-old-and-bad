bd3a80fc9bf01cfdb6c2f3b65cd4a204ccddeba4
Johnny Stene <jhonnystene@protonmail.com>
2021-06-08 16:33:40 -0600

    Update launcher to use new menu system

--- launcher.py
+++ launcher.py
@@ -58,37 +58,6 @@ if(__name__ == "__main__"):
         while True:
             key = KeyInput.getKey()
             if(key == "ok"):
-                inAppMenu = True
-                while inAppMenu:
-                    PhoneDisplay.drawAppHeader("Launcher")
-                    PhoneDisplay.drawNavbar("Back", "Open", "")
-                    PhoneDisplay.drawList(appNames, appSelected)
-                    #PhoneDisplay.drawToggleSwitch(40, 40, True)
-                    #PhoneDisplay.drawToggleSwitch(80, 40, False)
-                    #PhoneDisplay.drawButton("Button!", 40, 85, 60, 30)
-                    #PhoneDisplay.drawTextBox("Text box!", 40, 120, 150)
-                    PhoneDisplay.refresh()
-
-                    # Input handling goes here
-                    while True:
-                        key = KeyInput.getKey()
-
-                        if(key == "down"):
-                            appSelected += 1
-                            if(appSelected == len(appNames)):
-                                appSelected = 0
-                            break
-                        elif(key == "up"):
-                            appSelected -= 1
-                            if(appSelected < 0):
-                                appSelected = len(appNames) - 1
-                            break
-                        elif(key == "ok"):
-                            App.launchApp(apps[appSelected], PhoneDisplay, KeyInput, BuildInfo, UserSettings)
-                            appSelected = 0
-                            break
-                        elif(key == "end"):
-                            inAppMenu = False
-                            break
-                appSelected = 0
+                selected = PhoneDisplay.getMenuSelection(KeyInput, "Select App", appNames)
+                App.launchApp(apps[selected], PhoneDisplay, KeyInput, BuildInfo, UserSettings)
                 break
