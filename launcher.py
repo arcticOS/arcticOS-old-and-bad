00c14783effab5bd0a73afc06736798a2f085305
Johnny Stene <jhonnystene@protonmail.com>
2021-06-11 15:54:21 -0600

    Comment launcher.py

--- launcher.py
+++ launcher.py
@@ -18,46 +18,61 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-import json
-
 from arcticOS import HID, App, PreferenceStorage
 
 global BuildInfo, UserSettings
+# BuildInfo stores information that the user shouldn't normally need to change
 BuildInfo = PreferenceStorage.PreferenceStorage("settings/build.json")
+
+# The always_factory_reset key should be disabled in any production versions
 if(BuildInfo.getKey("always_factory_reset")):
     PreferenceStorage.factoryReset()
 
+# UserSettings stores anything that the user should normally need to change
 UserSettings = PreferenceStorage.PreferenceStorage("settings/user.json")
 
+# The HID file handles drivers for the keyboard, screen, and (soon) sound
 global PhoneDisplay, KeyInput
 PhoneDisplay = HID.Display()
 KeyInput = HID.KeyInput()
 
 if(__name__ == "__main__"):
-    # Launch OOBE if needed
+    # The out of box experience should be the initial setup process for the phone
+    # (Name, date/time, etc...)
+    # We use the OOBEComplete UserSettings key to determine whether or not this
+    # actually needs to be run
     if(UserSettings.getKey("OOBEComplete") == 0):
         print("Launching OOBE...")
         App.launchPath("apps/system/oobe.py", PhoneDisplay, KeyInput, BuildInfo, UserSettings)
 
         UserSettings.setKey("OOBEComplete", 1)
 
+    # We need to get a list of all apps, and then make another list to store
+    # just the names. We do this because getMenuSelection() only works with
+    # strings.
     apps = App.getAllApps("apps")
     appNames = []
     for app in apps:
         appNames.append(app.name)
     appSelected = 0
 
+    # Very basic main loop with an app selector.
     while True:
-        PhoneDisplay.clear() # TODO: get the actual time
+        PhoneDisplay.clear() \
+        # Show the time
+        # TODO: Get the time instead of using placeholder time
         PhoneDisplay.drawText("12:00 PM", 42, 10, 5)
         PhoneDisplay.drawText("Monday, June 07, 2021", 25, 10, 50)
+
+        # Draw the rest of the UI
         PhoneDisplay.drawNavbar("", "Apps", "")
         PhoneDisplay.refresh()
         
-        inAppMenu = False
+        # Wait for a key press
         while True:
             key = KeyInput.getKey()
             if(key == "ok"):
+                # Get and start an app with a menu
                 selected = PhoneDisplay.getMenuSelection(KeyInput, "Select App", appNames)
                 App.launchApp(apps[selected], PhoneDisplay, KeyInput, BuildInfo, UserSettings)
                 break
