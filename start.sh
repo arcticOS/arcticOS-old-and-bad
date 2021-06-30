e5ea7039b5142d28b1ab5798386a43f372b3bcf7
Johnny Stene <jhonnystene@protonmail.com>
2021-06-29 23:00:02 -0600

    Remove debug prints

--- start.sh
+++ start.sh
@@ -3,10 +3,12 @@ echo "Starting display service..."
 if ! screen -list | grep -q 'arctic-display'; then # make sure we don't start too many instances
     screen -dmS arctic-display bash -c "cd services/display && ./server.py"
     echo "Started display service."
-    sleep 2
 else
     echo "Display service already running."
 fi
+
+echo "Waiting to let services finish starting..."
+sleep 2
 echo "Starting launcher..."
 cd launcher
 python3 launcher.py
